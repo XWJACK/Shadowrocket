ff3616b9e19f1a3ea9eb305a389454044ed50a22
Jack <xuwenjiejack@gmail.com>
2019-01-07 21:00:48 +0800

    Completed

---
+++ Today/TodayViewController.h
@@ -0,0 +1,13 @@
+//
+//  TodayViewController.h
+//  Today
+//
+//  Created by Jack on 2019/1/7.
+//  Copyright © 2019 XWJACK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TodayViewController : UIViewController
+
+@end
---
+++ Today/TodayViewController.m
@@ -0,0 +1,33 @@
+//
+//  TodayViewController.m
+//  Today
+//
+//  Created by Jack on 2019/1/7.
+//  Copyright © 2019 XWJACK. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encountered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+@end
